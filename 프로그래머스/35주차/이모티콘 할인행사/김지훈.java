e738ff38f6fee2156b4e5705a02dd81a96cb4281
rhldkszns97 <sujeng98@naver.com>
2024-11-10 17:25:06 +0900

    35주차

---
+++ 프로그래머스/35주차/이모티콘 할인행사/김지훈.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+
+    // 각 이모티콘 할인률 10~40% 의 중복순열을 구해서 답을 계산한다.
+    int[] discounts = {10, 20, 30, 40};
+    int emoticonsLen = 0;
+
+    public int[] solution(int[][] users, int[] emoticons) {
+
+        emoticonsLen = emoticons.length;
+
+        List<int[]> cp = new ArrayList<>();
+        findCartesianProduct(cp, new int[emoticonsLen], 0);
+
+        List<int[]> amounts = new ArrayList<>();
+
+        int maxSubscriber = 0;
+        int maxSumPrice = 0;
+
+        for (int[] discountRates : cp) {
+            int[] amount = new int[users.length];
+            int subscriber = 0;
+            int sum = 0;
+            for (int i = 0; i < discountRates.length; i++) {
+                for (int j = 0; j < amount.length; j++) {
+                    int userRate = users[j][0];
+                    int discountRate = discountRates[i];
+                    if (discountRate >= userRate) {
+                        int sales = emoticons[i] * (100 - discountRate) / 100;
+                        amount[j] += sales;
+                        sum += sales;
+                    }
+                }
+            }
+
+            for (int i = 0; i < amount.length; i++) {
+                if (amount[i] >= users[i][1]) {
+                    subscriber++;
+                    sum -= amount[i];
+                }
+            }
+
+            if (subscriber > maxSubscriber) {
+                maxSubscriber = subscriber;
+                maxSumPrice = sum;
+            } else if (subscriber == maxSubscriber) {
+                if (sum > maxSumPrice) {
+                    maxSumPrice = sum;
+                }
+            }
+        }
+
+        return new int[]{maxSubscriber, maxSumPrice};
+    }
+
+    public void findCartesianProduct(List<int[]> cp, int[] discountList, int depth) {
+        if (depth == emoticonsLen) {
+            cp.add(Arrays.copyOf(discountList, discountList.length));
+            return;
+        }
+
+        for (int discount : discounts) {
+            discountList[depth] = discount;
+            findCartesianProduct(cp, discountList, depth + 1);
+        }
+    }
+}
