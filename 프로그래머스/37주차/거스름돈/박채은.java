8a067158b04f6d7869927ef8602c35ab237d262b
ChaeEun <64332359+cherry2030@users.noreply.github.com>
2024-11-24 21:51:09 +0900

    37주차

---
+++ 프로그래머스/37주차/거스름돈/박채은.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] money) {
+        int answer = 0;
+
+        int[] dp = new int[n+1];
+        Arrays.sort(money);
+
+        dp[0] = 1;
+
+        for(int i=0;i<money.length;i++){
+            for(int j=money[i];j<=n;j++){
+                dp[j] += dp[j- money[i]];
+            }
+        }
+
+        answer = (int) (dp[n] % 1000000007);
+
+        return answer;
+    }
+}
